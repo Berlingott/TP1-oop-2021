1ab8b9cd3b09e75b69e8a5d8b8d5a480a1fc713c
Berlingot <simsimondu@gmail.com>
2021-01-24 11:18:17 -0500

    Creation des vecteurs de pointeur dans la ligue

--- LigueSportive_class.h
+++ LigueSportive_class.h
@@ -3,11 +3,20 @@
 //
 
 #ifndef TP1_POO_2021_LIGUESPORTIVE_CLASS_H
+#include <vector>
+#include "Joueur_class.h"
+#include "Club_class.h"
+#include "Entraineur_class.h"
+#include "Stade_class.h"
 #define TP1_POO_2021_LIGUESPORTIVE_CLASS_H
 
 
 class LigueSportive_class {
 private:
+    std::vector<Joueur_class*> repertoireJoeur_vector;
+    std::vector<Joueur_class*> repertoireClub_vector;
+    std::vector<Entraineur_class*> repertoireEntraineur_vector;
+    std::vector<Stade_class*> repertoireStade_vector;
 protected:
 public:
 };
